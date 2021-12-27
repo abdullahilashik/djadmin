b1fdbfe8d61a5ccab891172ff01d68a8a3a06312
Ashik Arefin <abdullahilashik@gmail.com>
2021-12-27 08:18:17 +0600

    initialize

---
+++ djadmin/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils.text import slugify
+
+class JobCategory(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)    
+
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        
+        super(JobCategory, self).save(*args,**kwargs)
+    
+    def __str__(self):
+        return self.title
+
+class Location(models.Model):
+    title = models.CharField(max_length=250)
+
+
+class Job(models.Model):
+
+    JOB_LOCATION_CHOICES = (
+        ('dhaka','Dhaka'),
+        ('rajshahi','Rajshahi'),
+        ('chittagong',"Chittagong"),
+        ('khulna','Khulna'),
+        ('sylhet','Sylhet'),
+        ('all','All Bangladesh')
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    job_category = models.ForeignKey(JobCategory, on_delete=models.SET_NULL, null=True, blank=True)
+    min_salary = models.FloatField(default=0.0)
+    max_salary = models.FloatField(default=0.0)
+    location = models.ManyToManyField(Location)
+
+
+    def __str__(self):
+        return self.title
