ab543401c37162630f13b675ed2627346cea7db4
Ashik Arefin <abdullahilashik@gmail.com>
2021-12-31 23:00:52 +0600

    models

--- djadmin/admin.py
+++ djadmin/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import JobCategory, Job
+from django.http import HttpResponse
+import csv
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
@@ -7,10 +9,23 @@ class JobAdmin(admin.ModelAdmin):
 
 @admin.register(JobCategory)
 class JobCategoryAdmin(admin.ModelAdmin):
-    actions = ['delete_category']
+    actions = ['delete_category','export_as_csv']
 
     def delete_category(self, request, queryset):
         queryset.delete()
         return queryset
     
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        fieldnames = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+        self.message_user = 'CSV exported!'
+
+        return response
+
+    
+    export_as_csv.short_description = 'Export Selected'
     delete_category.short_description = 'Delete Categories'
